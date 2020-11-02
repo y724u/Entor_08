3bdb7d27a857b88584d4b688a44e7dad27ef89e0
y724u <yusukebebebe@gmail.com>
2020-11-02 10:42:08 +0900

    スムーススクロール実装

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-$(function() {
-  
 
+// <!-- スムーススクロール -->
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 700;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - 110;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
 });
