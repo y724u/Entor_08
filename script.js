654f574b5e949b7fb237413b413dc47dd16ef3cd
y724u <68385107+y724u@users.noreply.github.com>
2020-11-08 13:49:46 +0900

    js記述修正

--- script.js
+++ script.js
@@ -9,122 +9,55 @@ $(function () {
     $("html, body").animate({ scrollTop: position }, speed, "swing");
     return false;
   });
-});
 
-// <!-- サービスシステム開発モーダル -->
-$(function () {
+  // <!-- サービスシステム開発モーダル -->
   $('.js-modalOpen').on('click', function () {
     $('.js--modal').addClass('show_modal');
   });
   $('.js-modalClose').on('click', function () {
     $('.js--modal').removeClass('show_modal');
   });
-});
 
-// <!-- サービス地方創生モーダル -->
-$(function () {
+  // <!-- サービス地方創生モーダル -->
   $('.js-modalOpen2').on('click', function () {
     $('.js--modal2').addClass('show_modal2');
   });
   $('.js-modalClose').on('click', function () {
     $('.js--modal2').removeClass('show_modal2');
   });
-});
 
-// <!-- サービス人材育成モーダル -->
-$(function () {
+  // <!-- サービス人材育成モーダル -->
   $('.js-modalOpen3').on('click', function () {
     $('.js--modal3').addClass('show_modal3');
   });
   $('.js-modalClose').on('click', function () {
     $('.js--modal3').removeClass('show_modal3');
   });
-});
 
-// <!-- 利用規約モーダル -->
-$(function () {
+  // <!-- 利用規約モーダル -->
   $('.js-modalOpen-download').on('click', function () {
     $('.js--download__modal').addClass('show_modal');
   });
   $('.js-modalClose-download').on('click', function () {
     $('.js--download__modal').removeClass('show_modal');
   });
-});
 
-// <!-- サービスシステム開発モーダルスクロールストップ -->
-$(function () {
-  let scrollPosition;
-  $(".js-modalOpen").on("click", function () {
-    scrollPosition = $(window).scrollTop();
-    $('body').addClass('fixed').css({ 'top': -scrollPosition });
-  });
-  $(".js-modalClose").on("click", function () {
-    $('body').removeClass('fixed').css({ 'top': 0 });
-    window.scrollTo(0, scrollPosition);
-  });
-});
-
-// <!-- サービス地方創生モーダルスクロールストップ -->
-$(function () {
-  let scrollPosition;
-  $(".js-modalOpen2").on("click", function () {
-    scrollPosition = $(window).scrollTop();
-    $('body').addClass('fixed').css({ 'top': -scrollPosition });
-  });
-  $(".js-modalClose").on("click", function () {
-    $('body').removeClass('fixed').css({ 'top': 0 });
-    window.scrollTo(0, scrollPosition);
-  });
-});
-
-// <!-- サービス人材育成モーダルスクロールストップ -->
-$(function () {
-  let scrollPosition;
-  $(".js-modalOpen3").on("click", function () {
-    scrollPosition = $(window).scrollTop();
-    $('body').addClass('fixed').css({ 'top': -scrollPosition });
-  });
-  $(".js-modalClose").on("click", function () {
-    $('body').removeClass('fixed').css({ 'top': 0 });
-    window.scrollTo(0, scrollPosition);
-  });
-});
-
-// <!-- 利用規約モーダルストップ -->
-$(function () {
-  let scrollPosition;
-  $(".js-modalOpen-download").on("click", function () {
-    scrollPosition = $(window).scrollTop();
-    $('body').addClass('fixed').css({ 'top': -scrollPosition });
-  });
-  $(".js-modalClose-download").on("click", function () {
-    $('body').removeClass('fixed').css({ 'top': 0 });
-    window.scrollTo(0, scrollPosition);
-  });
-});
-
-// <!-- ダウンロードボタン活性化&非活性化 -->
-$(function () {
+  // <!-- ダウンロードボタン活性化&非活性化 -->
   $('input[name="checkbox"]').on('change', function () {
     if ($(this).prop('checked'))
       $('.js--button').addClass('box__checked');
     else
       $('.js--button').removeClass('box__checked');
   });
-});
 
-// <!-- ハンバーガーメニュー -->
-$(function () {
+  // <!-- ハンバーガーメニュー -->
   $('.js--hamburger').on('click', function () {
     $('.js-open').toggleClass('active')
   });
-});
 
-// <!-- ハンバーガーメニュースクールロールストップ -->
-$(function () {
+  // <!-- ハンバーガーメニュースクールロールストップ -->
   let state = false;
   let scrollpos;
-
   $('.js--hamburger').on('click', function () {
     if (state == false) {
       scrollpos = $(window).scrollTop();
@@ -138,7 +71,67 @@ $(function () {
       state = false;
     }
   });
+
+  // <!-- サービスシステム開発モーダルスクロールストップ -->
+
+  let scrollPosition1;
+  $(".js-modalOpen").on("click", function () {
+    scrollPosition1 = $(window).scrollTop();
+    $('body').addClass('fixed').css({ 'top': -scrollPosition1 });
+  });
+  $(".js-modalClose").on("click", function () {
+    $('body').removeClass('fixed').css({ 'top': 0 });
+    window.scrollTo(0, scrollPosition1);
+  });
+
+  // <!-- サービス地方創生モーダルスクロールストップ -->
+  let scrollPosition2;
+  $(".js-modalOpen2").on("click", function () {
+    scrollPosition2 = $(window).scrollTop();
+    $('body').addClass('fixed').css({ 'top': -scrollPosition2 });
+  });
+  $(".js-modalClose").on("click", function () {
+    $('body').removeClass('fixed').css({ 'top': 0 });
+    window.scrollTo(0, scrollPosition2);
+  });
+
+  // <!-- サービス人材育成モーダルスクロールストップ -->
+  let scrollPosition3;
+  $(".js-modalOpen3").on("click", function () {
+    scrollPosition3 = $(window).scrollTop();
+    $('body').addClass('fixed').css({ 'top': -scrollPosition3 });
+  });
+  $(".js-modalClose").on("click", function () {
+    $('body').removeClass('fixed').css({ 'top': 0 });
+    window.scrollTo(0, scrollPosition3);
+  });
+
+  // <!-- 利用規約モーダルストップ -->
+  let scrollPosition;
+  $(".js-modalOpen-download").on("click", function () {
+    scrollPosition = $(window).scrollTop();
+    $('body').addClass('fixed').css({ 'top': -scrollPosition });
+  });
+  $(".js-modalClose-download").on("click", function () {
+    $('body').removeClass('fixed').css({ 'top': 0 });
+    window.scrollTo(0, scrollPosition);
+  });
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
